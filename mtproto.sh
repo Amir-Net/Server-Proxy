7b56a4aaeffa464a920586d526422b31452fb05b
BabyBoss <151217448+Amir-Net@users.noreply.github.com>
2023-11-19 04:59:49 +0330

    Update mtproto.sh

--- mtproto.sh
+++ mtproto.sh
@@ -10,9 +10,23 @@ fi
 clear
 
  # Golang install
-  sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.4.linux-amd64.tar.gz
-  sudo export PATH=$PATH:/usr/local/go/bin
+  cd ~
+  sudo wget https://go.dev/dl/go1.21.4.linux-amd64.tar.gz
+  sudo tar -C /usr/local -xzf go1.21.4.linux-amd64.tar.gz
+  sed -i -e '$aexport PATH=$PATH:/usr/local/go/bin' ~/.profile
+  source ~/.profile
   go version
+  sudo rm go1.21.4.linux-amd64.tar.gz
 
  # MTG mtproto installer
- 
+  sudo wget https://github.com/9seconds/mtg/releases/download/v2.1.7/mtg-2.1.7-linux-amd64.tar.gz
+  sudo tar -C /usr/local/bin -xzf mtg-2.1.7-linux-amd64.tar.gz
+  sudo mv /usr/local/bin/mtg-2.1.7-linux-amd64 /usr/local/bin/mtg
+  sudo rm mtg-2.1.7-linux-amd64.tar.gz
+  git clone https://github.com/9seconds/mtg.git
+  cd mtg
+  make static
+
+  # MTG mtproto config
+  cd /usr/local/bin/mtg
+  
