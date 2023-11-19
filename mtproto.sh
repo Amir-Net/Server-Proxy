4ab895c5e1472438e61248a6f7a45b8c170fd8aa
BabyBoss <151217448+Amir-Net@users.noreply.github.com>
2023-11-19 07:39:30 +0330

    Update mtproto.sh

--- mtproto.sh
+++ mtproto.sh
@@ -1,11 +1,13 @@
-# Check for sudo privileges
+# Check for root privileges
 if [[ $EUID -ne 0 ]]; then
   if [[ $(sudo -n true 2>/dev/null) ]]; then
-    echo "This script will be run with sudo privileges."
+    echo "This script must be run with root access."
   else
-    echo "This script must be run with sudo privileges."
+    echo "This script must be run with root access."
     exit 1
   fi
+  su root
+  echo "Enter root password:"
 fi
 clear
 
@@ -17,30 +19,30 @@ sudo rm -rvf /etc/paths.d/go
   
  # Golang install
   cd ~
-  sudo wget https://go.dev/dl/go1.21.4.linux-amd64.tar.gz
-  sudo tar -C /usr/local -xzf go1.21.4.linux-amd64.tar.gz
+  wget https://go.dev/dl/go1.21.4.linux-amd64.tar.gz
+  tar -C /usr/local -xzf go1.21.4.linux-amd64.tar.gz
   sed -i -e '$aexport PATH=$PATH:/usr/local/go/bin' ~/.profile
   source ~/.profile
   go version
-  sudo rm go1.21.4.linux-amd64.tar.gz
+  rm go1.21.4.linux-amd64.tar.gz
 
  # MTG mtproto installer
-  sudo wget https://github.com/9seconds/mtg/releases/download/v2.1.7/mtg-2.1.7-linux-amd64.tar.gz
-  sudo tar -xzf mtg-2.1.7-linux-amd64.tar.gz
-  sudo cp mtg /usr/local/bin
-  sudo cp mtg /bin
+  wget https://github.com/9seconds/mtg/releases/download/v2.1.7/mtg-2.1.7-linux-amd64.tar.gz
+  tar -xzf mtg-2.1.7-linux-amd64.tar.gz
+  cp mtg-2.1.7-linux-amd64/mtg /usr/local/bin
+  cp  mtg-2.1.7-linux-amd64/mtg /bin
   chmod +x /usr/local/bin/mtg
   chmod +x /bin/mtg
-  sudo rm mtg-2.1.7-linux-amd64.tar.gz
-  
-  git clone https://github.com/9seconds/mtg.git
-  cd mtg
-  make static
+  rm mtg-2.1.7-linux-amd64.tar.gz
+  go install github.com/9seconds/mtg/v2@latest
 
   # MTG mtproto config
-  sudo wget -p /etc https://raw.githubusercontent.com/Amir-Net/Server-Proxy/main/mtg.toml
-  sudo wget -p /etc/systemd/system https://raw.githubusercontent.com/Amir-Net/Server-Proxy/main/mtg.service
-  sudo systemctl daemon-reload
-  sudo systemctl enable mtg
-  sudo systemctl start mtg
-  sudo systemctl status mtg
+  wget -p /etc https://raw.githubusercontent.com/Amir-Net/Server-Proxy/main/mtg.toml
+  wget -p /etc/systemd/system https://raw.githubusercontent.com/Amir-Net/Server-Proxy/main/mtg.service
+
+  # MTG mtproto runing
+  systemctl daemon-reload
+  systemctl enable mtg
+  systemctl start mtg
+  systemctl status mtg
+  mtg access /etc/mtg.toml
