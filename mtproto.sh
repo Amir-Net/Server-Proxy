7bcae6c80e057787a9a20092cbf1433fe0f854a6
BabyBoss <151217448+Amir-Net@users.noreply.github.com>
2023-11-20 02:47:01 +0330

    Update mtproto.sh

--- mtproto.sh
+++ mtproto.sh
@@ -11,21 +11,6 @@ if [[ $EUID -ne 0 ]]; then
 fi
 clear
 
-# Old Golang uninstall
-sudo apt-get -y remove golang
-sudo apt-get -y remove golang-go
-sudo rm -rvf /usr/local/go/
-sudo rm -rvf /etc/paths.d/go
-  
- # Golang install
-  cd ~
-  wget https://go.dev/dl/go1.21.4.linux-amd64.tar.gz
-  tar -C /usr/local -xzf go1.21.4.linux-amd64.tar.gz
-  sed -i -e '$aexport PATH=$PATH:/usr/local/go/bin' ~/.profile
-  source ~/.profile
-  go version
-  rm go1.21.4.linux-amd64.tar.gz
-
  # MTG mtproto installer
   wget https://github.com/9seconds/mtg/releases/download/v2.1.7/mtg-2.1.7-linux-amd64.tar.gz
   tar -xzf mtg-2.1.7-linux-amd64.tar.gz
@@ -34,7 +19,6 @@ sudo rm -rvf /etc/paths.d/go
   chmod +x /usr/local/bin/mtg
   chmod +x /bin/mtg
   rm mtg-2.1.7-linux-amd64.tar.gz
-  # go install github.com/9seconds/mtg/v2@latest
 
   # MTG mtproto config
   wget -P /etc https://raw.githubusercontent.com/Amir-Net/Server-Proxy/main/mtg.toml
