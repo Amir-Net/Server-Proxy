6a8ba3e248e03ff232487cbc638ca473f863c9f1
BabyBoss <151217448+Amir-Net@users.noreply.github.com>
2023-11-20 02:48:06 +0330

    Update golang.sh

--- golang.sh
+++ golang.sh
@@ -1,3 +1,16 @@
+# Check for root privileges
+if [[ $EUID -ne 0 ]]; then
+  if [[ $(sudo -n true 2>/dev/null) ]]; then
+    echo "This script must be run with root access."
+  else
+    echo "This script must be run with root access."
+    exit 1
+  fi
+  echo "Please enter root password and run script again."
+  su root
+fi
+clear
+
  # Old Golang uninstall
  apt-get -y remove golang
  apt-get -y remove golang-go
