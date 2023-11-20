81d81859e67e23db5871687509c89ad2cc34e8d9
BabyBoss <151217448+Amir-Net@users.noreply.github.com>
2023-11-21 03:20:39 +0330

    Update ipban.sh

--- ipban.sh
+++ ipban.sh
@@ -1,166 +1,29 @@
-#!/bin/bash
-# v2.0 github.com/AliDbg/IPBAN ######### Linux Debian11-12 - Ubuntu20-22
-#bash ./ipban.sh -install yes -io OUTPUT -geoip CN,IR -limit DROP -noicmp no
-#bash ./ipban.sh -add yes
-#bash ./ipban.sh -reset yes
-#bash ./ipban.sh -remove yes
-##################################################################
-GEOIP="CN,IR,CU,VN,ZW,BY";IO="x";LIMIT="x";INSTALL="n";RESET="n";REMOVE="n";ADD="n";NOICMP="x";release="";Src=""
-CHECK_OS(){
-	[[ $EUID -ne 0 ]] && echo "Run as root!" && exit 1
-	if [[ -f /etc/redhat-release ]]; then Src="yum";release="centos";
-	elif grep -Eqi "debian" /etc/issue; then Src="apt";release="debian";
-	elif grep -Eqi "ubuntu" /etc/issue; then Src="apt";release="ubuntu";
-	elif grep -Eqi "centos|red hat|redhat" /etc/issue; then Src="yum";release="centos";
-	elif grep -Eqi "debian|raspbian" /proc/version; then Src="apt";release="debian";
-	elif grep -Eqi "ubuntu" /proc/version; then Src="apt";release="ubuntu";
-	elif grep -Eqi "centos|red hat|redhat" /proc/version; then Src="yum";release="centos";
-	fi
-}
-# get arguments
-while [ "$#" -gt 0 ]; do
-  case "$1" in
-    -add) ADD="$2"; shift 2;;
-    -reset) RESET="$2"; shift 2;;
-    -remove) REMOVE="$2"; shift 2;;
-    -install) INSTALL="$2"; shift 2;;
-	-noicmp) NOICMP="$2"; shift 2;;
-    -geoip) GEOIP="$2"; shift 2;;
-    -limit) LIMIT="$2"; shift 2;;
-    -io) IO="$2"; shift 2;;
-    *) shift 1;;
-  esac
-done
+# Check for root privileges
+if [[ $EUID -ne 0 ]]; then
+  if [[ $(sudo -n true 2>/dev/null) ]]; then
+    echo "This script must be run with root access."
+  else
+    echo "This script must be run with root access."
+    exit 1
+  fi
+  echo "Please enter root password and run script again."
+  su root
+fi
+clear
 
-success() {
-	Green="\033[1;32m"
-	Font="\033[0m"
-	clear
-	iptables -vnL
-	echo -e "${Green}[+]${Font} $*"
-	exit 0
-}
-iptables_restart(){
-	service iptables restart
-	systemctl restart netfilter-persistent.service
-	sleep 1
-}
-iptables_reset_rules(){
-	iptables -F && iptables -X && iptables -Z
-	iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-	iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-}
-iptables_save_restart(){
-	iptables-save > /etc/iptables/rules.v4
-	iptables_restart
-}
-uninstall_ipban(){
-	crontab -l | grep -v "ipban-update.sh" | crontab -
-	systemctl stop netfilter-persistent.service && systemctl disable netfilter-persistent.service
-	iptables_reset_rules
-	rm /etc/iptables/rules.v4
-	iptables-save
-	rm -rf /usr/share/ipban/
-	success "Uninstalled IPBAN!"
-}
+# Block iran ip
+sudo apt-get update -y
+sudo apt-get -y upgrade
+sudo apt-get install curl unzip perl xtables-addons-common libtext-csv-xs-perl libmoosex-types-netaddr-ip-perl iptables-persistent -y 
+sudo mkdir /usr/share/xt_geoip
 
-download_build_dbip (){
-	mkdir -p /usr/share/ipban/ && chmod a+rwx /usr/share/ipban/
-cat > "/usr/share/ipban/download-build-dbip.sh" << EOF
-#!/bin/bash
-	dtmp="/usr/share/xt_geoip/tmp/"
-	rm -rf "\${dtmp}" && mkdir -p "\${dtmp}" && cd "\${dtmp}"
-	# Download dbipLite Full
-	timestamp=\$(date "+%Y-%m")
-	curl -m 9 -fLO "https://download.db-ip.com/free/dbip-country-lite-\${timestamp}.csv.gz" &> /dev/null
-	curl -m 9 -fLO "https://mailfud.org/geoip-legacy/GeoIP-legacy.csv.gz" -O &> /dev/null
-	if [[ "\$?" != 0 ]]; then
-	curl -m 9 -fLO "https://legacy-geoip-csv.ufficyo.com/Legacy-MaxMind-GeoIP-database.tar.gz" &> /dev/null
-	fi		
-	# Combine all csv and remove duplicates 
-	gzip -dfq *.gz
-	find . -name "*.tar" -exec tar xvf {} \;
-	find . -name "*.tar.gz" -exec tar xvzf {} \;
-	cat "\${dtmp}GeoIP-legacy.csv" | tr -d '"' | cut -d, -f1,2,5 > "\${dtmp}GeoIP-legacy-processed.csv"
-	rm "\${dtmp}GeoIP-legacy.csv"
-	cat *.csv > geoip.csv 
-	sort -u geoip.csv -o "/usr/share/xt_geoip/dbip-country-lite.csv"
-	rm -rf "\${dtmp}"
-EOF
-	chmod +x "/usr/share/ipban/download-build-dbip.sh"
-}
+sudo wget -4 -O /usr/local/bin/geo-update.sh https://raw.githubusercontent.com/Amir-Net/Server-Proxy/main/ipgeo.sh
 
-create_update_sh(){
-mkdir -p /usr/share/ipban/ && chmod a+rwx /usr/share/ipban/
-cat > "/usr/share/ipban/ipban-update.sh" << EOF
-#!/bin/bash
-	/usr/share/ipban/download-build-dbip.sh
-	cd /usr/share/xt_geoip/
-	/usr/libexec/xtables-addons/xt_geoip_build -s
-	/usr/lib/xtables-addons/xt_geoip_build -D /usr/share/xt_geoip/
-	cd && rm /usr/share/xt_geoip/dbip-country-lite.csv
-	service iptables restart
-	systemctl restart netfilter-persistent.service
-	clear && echo "Updated IPBAN!" 
-EOF
-chmod +x "/usr/share/ipban/ipban-update.sh"
-}
+chmod 755 /usr/lib/xtables-addons/xt_geoip_build
+bash /usr/local/bin/geo-update.sh
 
-iptables_rules(){
-	if [[ ${NOICMP} == *"y"* ]]; then
-		iptables -A INPUT -p icmp -j DROP
-	fi	
-	
-	if [[ ${IO} == *"I"* ]]; then
-		iptables -A INPUT -m geoip --src-cc "${GEOIP}" -j "${LIMIT}"
-	fi
-	
-	if [[ ${IO} == *"O"* ]]; then
-		iptables  -A OUTPUT -m geoip --dst-cc "${GEOIP}" -j "${LIMIT}"
-	fi
-		
-	if [[ ${IO} == *"F"* ]]; then
-		iptables -A FORWARD -m geoip --dst-cc "${GEOIP}" -j "${LIMIT}"
-	fi
-}
-
-install_ipban(){
-	CHECK_OS
-	$Src -y update
-	$Src -y install curl gzip tar perl xtables-addons-common xtables-addons-dkms libtext-csv-xs-perl libmoosex-types-netaddr-ip-perl iptables-persistent libnet-cidr-lite-perl
-	if [[ "${release}" == "debian" ]]; then
-		$Src -y install module-assistant xtables-addons-source
-		module-assistant prepare
-		module-assistant -f auto-install xtables-addons-source
-	fi	
-	rm -rf /usr/share/xt_geoip/ && mkdir -p /usr/share/xt_geoip/ && chmod a+rwx /usr/share/xt_geoip/
-	modprobe x_tables && modprobe xt_geoip
-	chmod +x /usr/lib/xtables-addons/xt_geoip_build
-	chmod +x /usr/libexec/xtables-addons/xt_geoip_dl
-	iptables_restart
-	systemctl enable cron && ufw disable
-	crontab -l | grep -v "ipban-update.sh" | crontab -
-	(crontab -l 2>/dev/null; echo "$(shuf -i 1-59 -n 1) $(shuf -i 1-23 -n 1) * * * bash /usr/share/ipban/ipban-update.sh >/dev/null 2>&1") | crontab -
-	download_build_dbip
-	create_update_sh && bash "/usr/share/ipban/ipban-update.sh"	
-	iptables_reset_rules
-	iptables_rules
-	systemctl enable netfilter-persistent.service
-	iptables_save_restart
-	success "Installed IPBAN!"
-}
-add_ipban(){
-	iptables_rules
-	iptables_save_restart
-	success "Added Rules!"
-}
-reset_iptables(){
-	iptables_reset_rules
-	iptables_save_restart
-	success "Resetted IPTABLES!"
-}
-if [[ ${RESET} == *"y"* ]]; then reset_iptables; fi
-if [[ ${ADD} == *"y"* ]]; then add_ipban; fi
-if [[ ${REMOVE} == *"y"* ]]; then uninstall_ipban; fi
-if [[ ${INSTALL} == *"y"* ]]; then install_ipban; fi
-#### END.
+sudo iptables -A OUTPUT -m geoip -p tcp --destination-port 80 --dst-cc IR -j DROP
+sudo iptables -A OUTPUT -m geoip -p tcp --destination-port 443 --dst-cc IR -j DROP
+iptables-save
+clear
+echo -e "Blocked Port 80 and 443 IRAN \n"
