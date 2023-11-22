8e907b9a6d13646240789f5282062981ca508114
BabyBoss <151217448+Amir-Net@users.noreply.github.com>
2023-11-23 00:12:27 +0330

    Create  mtproto-py.sh

---
+++ mtproto-py.sh
@@ -0,0 +1,14 @@
+# Check for root privileges
+if [[ $EUID -ne 0 ]]; then
+  if [[ $(sudo -n true 2>/dev/null) ]]; then
+    echo "This script must be run with root access."
+  else
+    echo "This script must be run with root access."
+    exit 1
+  fi
+  echo "Please enter root password and run script again."
+  su root
+fi
+clear
+
+# Python mtproto installer
