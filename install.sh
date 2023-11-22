7d5ef6ff9fcd8a6327f46b4c94c584d3237e9fa5
BabyBoss <151217448+Amir-Net@users.noreply.github.com>
2023-11-22 23:30:43 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -44,16 +44,18 @@ system_update() {
   fi
 }
 
-# 2-Function to IP-baning
+# 2. Function to ban IP system
 system_ipban() {
-  dialog --title "System IP-baning" --yesno "This operation will IP-ban system. Do you want to proceed?" 10 60
+  dialog --title "System IP Ban" --yesno "This operation will ban your system IP. Do you want to proceed?" 10 60
   response=$?
   
   if [ $response -eq 0 ]; then
+
   bash -c "$(curl -Lfo- https://raw.githubusercontent.com/Amir-Net/Server-Proxy/main/ipban.sh)"
-    dialog --msgbox "IP-baning completed." 10 60
+
+    dialog --msgbox "System updates and cleanup completed." 10 60
   else
-    dialog --msgbox "SIP-baning operation canceled." 10 60
+    dialog --msgbox "System updates and cleanup operation canceled." 10 60
   fi
 }
 
